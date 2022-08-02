88e6d76fc0cbd9a49ad0e1d7e2f0b19672921e77
jaeyoon lee <jaeyoon@jaeyoons-MacBook-Air.local>
2022-08-02 20:49:31 +0900

    clear all projects

--- Chapter6/src/sec06/exam03/package1/B.java
+++
@@ -1,5 +0,0 @@
-package sec06.exam03.package1;
-
-public class B {
-    A a; //class A는 같은 패키지 안에 있는 default 로 선언된 클래스이다.
-}
--- Chapter6/src/sec06/exam03/package2/C.java
+++
@@ -1,8 +0,0 @@
-package sec06.exam03.package2;
-
-import sec06.exam03.package1.*;
-
-public class C {
-    // A a; A는 다른 패키지(package1)에서 default로 선언됐기 때문에 여기서 사용 불가능
-    B b; //B는 다른 패키지(package1)에서 public으로 선언됐기 때문에 여기서 사용 가능하다.
-}
